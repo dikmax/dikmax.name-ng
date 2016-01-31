32debcfbd9e1d90ece215c8dc2a33fc976d368c9
Maxim Dikun <dikmax@gmail.com>
2016-01-31 18:24:32 +0100

    Fixes to sidebar.

--- simple.js
+++ simple.js
@@ -1,7 +1,15 @@
-document.getElementsByClassName('side-menu-button')[0].addEventListener('click', function (e) {
+document.querySelector('.side-menu-button').addEventListener('click', function (e) {
     e.stopImmediatePropagation();
     e.preventDefault();
 
     document.querySelector('.sidebar').classList.add('active');
+    return false;
+});
+
+document.querySelector('.sidebar').addEventListener('click', function (e) {
+    e.stopImmediatePropagation();
+    e.preventDefault();
+
+    document.querySelector('.sidebar').classList.remove('active');
     return false;
 });
