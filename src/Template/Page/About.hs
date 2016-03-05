6a54adc5100b183de1a53dede92ade06d0e15a32
Maxim Dikun <dikmax@gmail.com>
2016-03-05 18:23:43 +0100

    Rest of about page.

--- src/Template/Page/About.hs
+++ src/Template/Page/About.hs
@@ -5,8 +5,25 @@ import           Control.Lens
 import           Data.Default
 import           Lucid
 import           Template.Navigation
+import           Template.SvgIcons
 import           Text.Pandoc.LucidWriter
 import           Types
+
+buttons :: [(Text, Text, Text, Html ())]
+buttons =
+    [ ("E-mail",   "email",       "mailto:me@dikmax.name",                               iconEmail)
+    , ("Facebook", "facebook",    "https://www.facebook.com/dikmax",                     iconFacebook)
+    , ("Google+",  "google-plus", "https://www.google.com/+MaximDikun",                  iconGooglePlus)
+    , ("LinkedIn", "linkedin",    "https://www.linkedin.com/pub/maxim-dikun/36/389/303", iconLinkedIn)
+    , ("VK",       "vk",          "https://vk.com/dikmax",                               iconVk)
+    , ("last.fm",  "lastfm",      "http://www.last.fm/user/dikmax",                      iconLastfm)
+    , ("Instagram","instagram",   "https://www.instagram.com/dikmax/",                   iconInstagram)
+    , ("GitHub",   "github",      "https://github.com/dikmax",                           iconGitHub)
+    , ("StackOverflow","stack-overflow","https://stackoverflow.com/users/682727/dikmax", iconStackOverflow)
+    , ("Twitter",  "twitter",     "https://twitter.com/dikmax",                          iconTwitter)
+    , ("YouTube",  "youtube",     "https://www.youtube.com/user/zabeydikmax",            iconYouTube)
+    ]
+    -- <a href="https://www.youtube.com/user/zabeydikmax" title="YouTube" class="btn btn-youtube"><i class="fa fa-youtube fa-2x fa-fw"></i></a>
 
 
 aboutPage :: (Html () -> Html ()) -> File -> Html ()
@@ -14,6 +31,14 @@ aboutPage layout about = layout $ do
     navigation
     div_ [class_ "main main_no-hero"] $ do
         writeLucid opts $ about ^. fileContent
+
+        h1_ [class_ "main__centered post__block post__block_header-1"] $
+            toHtmlRaw ("Контакты" :: Text)
+        div_ [class_ "main__centered post__block post__block_div about__contacts"] $
+            forM_ buttons $ \(title, cl, link, icon) ->
+                a_ [ href_ link
+                   , title_ title
+                   , class_ $ "about__contact about__contact_" ++ cl] icon
     where
         opts :: LucidWriterOptions
         opts = def & showFigureNumbers .~ False
