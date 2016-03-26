4bde26b3d3623a23b8488261f900001fe1887fa1
Maxim Dikun <dikmax@gmail.com>
2016-03-27 00:23:20 +0100

    Einunddressig

--- src/Template/Layout.hs
+++ src/Template/Layout.hs
@@ -83,6 +83,8 @@ layout scripts cd meta content = doctypehtml_ $ do
         meta_ [term "property" "og:locale", content_ "ru_BY"]
         meta_ [term "property" "fb:profile_id", content_ "1201794820"]
 
+        googleAnalytics
+
     body_ $ do
         content
         footer
@@ -148,3 +150,18 @@ footer =
         div_ [class_ "footer__container"] $
             toHtmlRaw ("&copy; Максим Дикун, 2012 &mdash; 2016<br/>\
                 \Любимый корректор: Анастасия Барбосова" :: Text)
+
+googleAnalytics :: Html ()
+googleAnalytics =
+    -- TODO move UA to config
+    toHtmlRaw $ ("<script type=\"text/javascript\">\
+                     \var _gaq = _gaq || [];\
+                     \_gaq.push(['_setAccount', 'UA-32213724-1']);\
+                     \_gaq.push(['_setDomainName', 'dikmax.name']);\
+                     \_gaq.push(['_trackPageview']);\
+                     \(function() {\
+                         \var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;\
+                         \ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';\
+                         \var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);\
+                     \})();\
+                 \</script>" :: Text)
