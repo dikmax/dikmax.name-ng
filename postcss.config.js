b1e2e7ec4ba0db525fd5950a7f8f5af17d5bf585
Maxim Dikun <dikmax@gmail.com>
2019-11-20 22:18:22 +0100

    Update packages

--- postcss.config.js
+++ postcss.config.js
@@ -7,20 +7,18 @@ module.exports = {
   plugins: [
     require('postcss-import')({}),
     require('postcss-mixins')({}),
+    require('postcss-custom-properties')({
+      preserve: false,
+    }),
+    require('postcss-custom-media')({}),
     require('postcss-preset-env')({
       browsers,
-      features: {
-        'custom-properties': {
-          preserve: false
-        }
-      }
     }),
     require('colorguard')({
       'ignore': ['#000000', '#ffffff', '#212121', '#b3b3b3']
     }),
     require('postcss-quantity-queries')({}),
     require('postcss-nested')({}),
-    require('postcss-custom-media')({}),
     require('lost')({}),
     require('postcss-responsive-type')({}),
     require('postcss-short')({
